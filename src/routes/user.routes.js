0f30a74a27129868e0c4c0187d9b0c56a61dd76f
nirob <nirobprottoy12@gmail.com>
2026-03-31 01:25:25 +0600

    Refactor user.routes.js: add secured routes for changing password, retrieving current user, and updating account details

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,9 +4,17 @@ import {
   logoutUser,
   registerUser,
   refreshAccessToken,
+  changeCurrentPassword,
+  getCurrentUser,
+  UpdateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
+  getUserChannelProfile,
+  getWatchHistory,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
 const router = Router();
 
@@ -26,9 +34,23 @@ router.route("/register").post(
 
 router.route("/login").post(loginUser);
 
-
 //secured routes
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+
+//patch
+router.route("/update-account").patch(verifyJWT, UpdateAccountDetails);
+router
+  .route("/avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router
+  .route("/cover-image")
+  .patch(verifyJWT, upload.single("cover-image"), updateUserCoverImage);
+
+//here params is used to get the username of the channel which is being accessed by the user.
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/history").get(verifyJWT, getWatchHistory);
 
 export default router;
